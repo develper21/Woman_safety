2c4ad7ce6494b81eebe282bfc1bf4219a135e103
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-12 22:28:19 +0530

    file: upload a mobile app metro.config.js file

---
+++ mobile/metro.config.js
@@ -0,0 +1,29 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    alias: {
+      '@': './src',
+      '@components': './src/components',
+      '@screens': './src/screens',
+      '@services': './src/services',
+      '@utils': './src/utils',
+      '@types': './src/types',
+      '@constants': './src/constants',
+      '@hooks': './src/hooks',
+      '@store': './src/store',
+    },
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
